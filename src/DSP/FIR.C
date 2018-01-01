6cbbb0e21987ade84f062eee06785f66959a2411
Matt Flax <flatmax@flatmax.org>
2018-01-01 15:26:35 +1100

    Added overlap add FIR.

---
+++ src/DSP/FIR.C
@@ -0,0 +1,62 @@
+/* Copyright 2000-2018 Matt Flax <flatmax@flatmax.org>
+   This file is part of GTK+ IOStream class set
+
+   GTK+ IOStream is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GTK+ IOStream is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You have received a copy of the GNU General Public License
+   along with GTK+ IOStream
+*/
+
+#include <Sox.H>
+
+#include "DSP/FIR.H"
+
+int FIR::loadTimeDomainCoefficients(const std::string fileName){
+  int ret=NO_ERROR;
+  Sox<FP_TYPE> sox; // use sox to try to read the filter from file
+  if ((ret=sox.openRead(string(fileName)))<0 && ret!=SOX_READ_MAXSCALE_ERROR) // try to open the file
+      SoxDebug().evaluateError(ret, fileName);
+  else {
+      Eigen::Matrix<FP_TYPE, Eigen::Dynamic, Eigen::Dynamic> hNew; // the time domain representation of the filter
+      if (ret=sox.read(hNew)<0) // Try to read the entire file into the coefficient Matrix B.
+          SoxDebug().evaluateError(ret, fileName);
+      else
+          loadTimeDomainCoefficients(hNew);
+  }
+  return ret;
+}
+
+void FIR::loadTimeDomainCoefficients(const Eigen::Matrix<FP_TYPE, Eigen::Dynamic, Eigen::Dynamic> hIn){
+  h=hIn;
+  resetDFT();
+}
+
+void FIR::resetDFT(){
+  // only reset the DFT if both block size and filter h are defined.
+  if (N==0 || h.rows()<=0 || h.cols() <=0)
+    return;
+  // Find the DFT of h and store in H
+  Eigen::Matrix<FP_TYPE, Eigen::Dynamic, Eigen::Dynamic> hNew(h.rows()+N, h.cols());
+  x.setZero(hNew.rows(), hNew.cols()); // make the input signal the same length as H
+  yTemp.setZero(hNew.rows(), 1); // make the temporary output buffer the same length as H
+  y.setZero(hNew.rows(), hNew.cols()); // make the temporary output buffer the same length as H
+  Y.setZero(hNew.rows(), 1); // make the DFT of the input signal the same length as H
+  hNew.setZero();
+  hNew.topRows(h.rows())=h;
+  H.setZero(hNew.rows(), hNew.cols());
+  for (int i=0; i<hNew.cols(); i++)
+    fft.fwd(H.col(i).data(), hNew.col(i).data(), hNew.rows());
+}
+
+void FIR::init(unsigned int blockSize){
+  N=blockSize;
+  resetDFT();
+}
---
+++ test/FIRTest.C
@@ -0,0 +1,66 @@
+/* Copyright 2000-2018 Matt Flax <flatmax@flatmax.org>
+   This file is part of GTK+ IOStream class set
+
+   GTK+ IOStream is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GTK+ IOStream is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You have received a copy of the GNU General Public License
+   along with GTK+ IOStream
+*/
+#include "DSP/FIR.H"
+#include <Octave.H>
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]){
+
+    int N=100;
+    int chCnt=1;
+
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> h;
+    h=Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>::Random(N,chCnt);
+
+    FIR fir;
+
+    int Mx=10;
+    int Nx=Mx*200;
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> x, y;
+    x=Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>::Random(Nx,chCnt);
+    y.setZero(x.rows(), x.cols());
+
+    fir.init(Mx);
+    fir.loadTimeDomainCoefficients(h);
+    for (int i=0; i<Nx/Mx; i++)
+      fir.filter(x.block(i*Mx, 0, Mx, chCnt), y.block(i*Mx, 0, Mx, chCnt));
+
+    Octave octave; /// The octave instance
+    vector<string> args(1); args[0]=string("");
+    octave.startOctave(args);
+
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> yHat, A;
+    A.setOnes(1,1);
+    yHat.setZero(x.rows(), x.cols());
+    for (int i=0; i<chCnt; i++){
+        vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> > input, output; // The octave input and output
+        input.push_back(h.col(i));
+        input.push_back(A);
+        input.push_back(x.col(i));
+        octave.runM("filter", input, output);
+        yHat.col(i)=output[0];
+    }
+
+    double err=(y-yHat).array().abs().sum()/(double)y.rows()/(double)y.cols();
+    double rms=x.array().abs().sum()/(double)x.rows()/(double)x.cols();
+    cout<<"rms="<<rms<<endl;
+    cout<<"err="<<err<<endl;
+    cout<<"err/rms="<<err/rms<<endl;
+    cout<<"error = "<<20.*log10(err/rms)<<" dB"<<endl;
+
+    return 0;
+}
