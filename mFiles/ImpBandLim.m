3f3e25685ac30b9876134752b5f86af1ad2ef8c7
Matt Flax <flatmax@flatmax.org>
2018-07-20 09:18:52 +1000

    Added a band limited impulse class.

---
+++ mFiles/ImpBandLim.m
@@ -0,0 +1,59 @@
+%/* Copyright 2000-2018 Matt Flax <flatmax@flatmax.org>
+%   This file is part of GTK+ IOStream class set
+%
+%   GTK+ IOStream is free software; you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation; either version 2 of the License, or
+%   (at your option) any later version.
+%
+%   GTK+ IOStream is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You have received a copy of the GNU General Public License
+%   along with GTK+ IOStream
+%*/
+
+function x=ImpBandLim(s, fs, fi, fa)
+% Generate a band limited impulse
+% Inputs :
+%         s : duration in s
+%         fs: sample rate in Hz
+%         fi: Minimum frequency in Hz
+%         fa: Maximum frequency in Hz
+% Output :
+%         x : The signal
+
+N=s*fs;
+x=zeros(N,1); % zeros of correct duration
+x(1)=1.0; % the impulse
+X=fft(x); % zero out of band data in the DFT domain
+f=linspace(0,fs-fs/N,N);
+idxOrig=find(f<fi | f>(fs-fi));
+idx=[1:fi/(fs/N) N-fi/(fs/N)+2:N];
+X(idx,1)=0;
+idxOrig=find(f>fa & f<(fs-fa));
+idx=[fa/(fs/N)+2:(fs-fa)/(fs/N)];
+X(idx,1)=0;
+% circular shift
+x=ifft(X);
+% don't worry about circularly shifting for now ...
+% xHat=circshift(x,N/2);
+% phi=(linspace(0,N-1,N))*pi/2;
+% X=X.*exp(-j*phi.');
+% xHat=ifft(X);
+%     subplot(211)
+%     plot(real(xHat))
+%     subplot(212)
+%     plot(imag(xHat))
+if ~isreal(x)
+    clf
+    subplot(211)
+    plot(real(x))
+    subplot(212)
+    plot(imag(x))
+  error('x should be a real signal');
+end
+clf; plot(x)
+end
---
+++ src/DSP/ImpulseBandLimited.C
@@ -0,0 +1,73 @@
+/* Copyright 2000-2018 Matt Flax <flatmax@flatmax.org>
+   This file is part of GTK+ IOStream class set
+
+   GTK+ IOStream is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GTK+ IOStream is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You have received a copy of the GNU General Public License
+   along with GTK+ IOStream
+*/
+
+#include "DSP/ImpulseBandLimited.H"
+#include <limits>
+
+// #ifdef HAVE_SOX
+// #include <Sox.H>
+//
+// template<typename FP_TYPE>
+// int FIR<FP_TYPE><FP_TYPE>::saveToFile(const std::string fileName){
+//   Sox<FP_TYPE> sox; // use sox to try to read the filter from file
+//   if ((ret=sox.openRead(string(fileName)))<0 && ret!=SOX_READ_MAXSCALE_ERROR) // try to open the file
+//       return SoxDebug().evaluateError(ret, fileName);
+//   else
+//     return sox.save
+//   return ret;
+// }
+// #endif
+
+template<typename FP_TYPE>
+int ImpulseBandLimited<FP_TYPE>::generateImpulse(float s, float fs, float fi, float fa){
+  if (s<=0. || fs<=0. || fi<0. || fa>fs/2.)
+    return Debug().evaluateError(EINVAL, "Duration is incorrect, ensure s>0");
+  if (fs<=0.)
+    return Debug().evaluateError(EINVAL, "Sample rate is incorrect, ensure fs>0");
+  if (fi>fs/2 || fi<0)
+    return Debug().evaluateError(EINVAL, "Minimum frequency is incorrect, ensure fs/2>=fi>=0");
+  if (fa>fs/2. || fa<0)
+    return Debug().evaluateError(EINVAL, "Maximum frequency is incorrect, ensure fs/2>=fa>=0");
+
+  int N=round(s*fs); // the number of samples
+  Eigen::Array<double, Eigen::Dynamic, 1> x(N,1);
+  x.setZero(); // initialise the impulse
+  x(0,0)=1.;
+  Eigen::Array<typename Eigen::FFT<double>::Complex, Eigen::Dynamic, 1> X(x.rows(), 1); // The DFT of the x
+  Eigen::FFT<double> fft; // The fast Fourier transform
+  fft.fwd(X.data(), x.data(), x.rows()); // find the DFT of x
+
+  // zero out of band data
+  int cnt=(int)(fi/(fs/(float)N))-1;
+  X.block(0,0,cnt,1).setZero(); // low freq
+  int start=(N-(int)(fi/(fs/(float)N))+1);
+  cnt=(N-1)-start+1;
+  X.block(start,0,cnt,1).setZero(); // high freq
+  start=(int)(fa/(fs/(float)N))+1;
+  cnt=(int)((fs-fa)/(fs/(float)N))-1-start+1;
+  X.block(start,0,cnt,1).setZero(); // mid freq
+
+  fft.inv(x.data(), X.data(), X.rows()); // find the iDFT of X
+  if (0. == (double)((FP_TYPE)0.1)) // scale if necessary (i.e. FP_TYPE is not a floating point type)
+    x*=std::numeric_limits<FP_TYPE>::max();
+  *(Eigen::Array<FP_TYPE, Eigen::Dynamic, 1>*)this=x.cast<FP_TYPE>(); // copy over
+  return 0;
+}
+
+template class ImpulseBandLimited<int>;
+// template class ImpulseBandLimited<unsigned int>;
+// template class ImpulseBandLimited<float>;
